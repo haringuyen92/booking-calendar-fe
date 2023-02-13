e599ecc1917d18a42dfee81fbc906e4d522366cb
congtd <congtd@mor.com.vn>
2023-02-13 15:07:27 +0700

    auth service API

---
+++ src/services/authService.js
@@ -0,0 +1,23 @@
+import httpClient from "../httpClient";
+
+class Auth{
+    async login(email, password){
+        console.log("start login");
+        try{
+            return await httpClient.post('/auth/login', {email, password});
+        }catch(err){
+            console.log(err);
+        }
+    }
+    async register(name, email, password){
+        console.log("start register");
+        try{
+            return await httpClient.post('/auth/register', {name, email, password})
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
+export default new Auth();
+ 
+
