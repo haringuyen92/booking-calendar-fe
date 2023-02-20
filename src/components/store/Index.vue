88b387383de8c64cdea5d2a12e1a6bf07a5a0d45
congtd <congtd@mor.com.vn>
2023-02-20 10:03:55 +0700

    store: getList

--- src/components/store/Index.vue
+++ src/components/store/Index.vue
@@ -1,64 +1,25 @@
-
-
-<!--<script >-->
-
-<!--// import BaseDataTable from "@/components/table/BaseDataTable.vue";-->
-
-<!--export default {-->
-<!--  async setup(){-->
-
-<!--    const test = ref();-->
-<!--    // const dataTable = reactive({-->
-<!--    //   columns: ['name', 'description'],-->
-<!--    //   rows: [-->
-<!--    //     { id: 1, name: 'John Doe', email: 'john@doe.com' },-->
-<!--    //     { id: 2, name: 'Jane Doe', email: 'jane@doe.com' }-->
-<!--    //   ],-->
-<!--    // })-->
-<!--    // const { data, success } = await StoreService.getAll();-->
-
-<!--    // if(success){-->
-<!--    //   dataTable.rows = [...data];-->
-<!--    // }-->
-<!--    // console.log(data);-->
-<!--    // console.log(success);-->
-<!--    onMounted(() => {-->
-<!--      console.log(test.value);-->
-<!--    })-->
-<!--    return {-->
-
-<!--    }-->
-<!--  }-->
-<!--}-->
-
-
-<!--</script>-->
-
 <template>
   <div ref="el">
     <div>
       <button type="button" class="btn btn-primary">New Store</button>
     </div>
-        <BaseDataTable :columns="dataTable.columns" :rows="dataTable.rows" />
+    <Suspense>
+      <BaseDataTable :columns="dataTable.columns" :rows="dataTable.rows" />
+    </Suspense>
   </div>
 </template>
 <script setup>
-  import {
-    // ref,
-    reactive,
-    // onMounted
-  } from 'vue';
+  import { reactive } from 'vue';
   import StoreService from "@/services/storeService";
   import BaseDataTable from "@/components/table/BaseDataTable.vue";
   const dataTable = reactive({
     columns: ['name', 'description', 'address', 'phone'],
     rows: [],
   })
-  StoreService.getAll().then(res => {
+  const getListStore = async () => {
+    const res = await StoreService.getAll();
     dataTable.rows = [...res.data];
-  }).catch(err => {
-    console.log(err);
-  })
-
+  }
+  getListStore();
 
 </script>
--- src/services/storeService.js
+++ src/services/storeService.js
@@ -2,7 +2,11 @@ import httpClient from "../httpClient";
 
 class StoreService{
     async getAll(){
-        return await httpClient.get('/stores');
+        try {
+            return await httpClient.get('/stores');
+        }catch (e){
+            new Error(e);
+        }
     }
 }
 
