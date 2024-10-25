7e0ce50a66299d12b77f8823021b32910981412d
hari <dev.hainguyen92@gmail.com>
2024-10-25 17:07:16 +0700

    Init booking calendar fe

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Hello, Tailwind CSS + React + TypeScript with Yarn!
+        </h1>
+      </div>
   );
 }
 
